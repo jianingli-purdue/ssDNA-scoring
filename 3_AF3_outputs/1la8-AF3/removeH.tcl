4d816e00d125791e79ff52b1f99fd34eec95bfc9
s2315288 <148905237+s2315288@users.noreply.github.com>
2026-01-05 14:10:56 -0500

    Add files via upload

---
+++ 3_AF3_outputs/1la8-AF3/removeH.tcl
@@ -0,0 +1,26 @@
+# strip_H.tcl
+# Usage:
+#   vmd -dispdev text -eofexit -e removeH.tcl -args native.pdb native_noH.pdb
+
+# Get arguments
+set in_file  [lindex $argv 0]
+set out_file [lindex $argv 1]
+
+# Load structure
+mol new $in_file waitfor all
+
+# Select all non-hydrogen atoms
+#set sel [atomselect top "all and noh and not (index 1 2 3)"]
+set sel [atomselect top "all and noh"]
+
+# Write out H-stripped PDB
+$sel writepdb $out_file
+
+# Clean up
+$sel delete
+mol delete top
+
+# Exit VMD
+exit
+
+
