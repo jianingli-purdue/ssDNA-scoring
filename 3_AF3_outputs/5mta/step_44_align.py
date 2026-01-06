cab955700b43b5a03e00545d62ecf564cabcec78
s2315288 <148905237+s2315288@users.noreply.github.com>
2026-01-06 12:33:19 -0500

    Add files via upload

---
+++ 3_AF3_outputs/5mta/step_44_align.py
@@ -0,0 +1,90 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+
+import MDAnalysis as mda
+from MDAnalysis.tests.datafiles import GRO, TRR, PSF, DCD
+from MDAnalysis.analysis import rms
+from MDAnalysis.analysis import distances, contacts, align
+from MDAnalysis.analysis.distances import distance_array
+
+import sys, os
+
+cutoffs = [2., 3., 4., 5.]
+
+input_gro = 'out_0.gro'
+input_trr = 'out_gro.trr'
+
+# --- Get indices as integers ---
+ref_ndx = int(input("please enter reference frame index: \n"))
+tag_ndx = int(input("please enter target frame index: \n"))
+print("reference index:", ref_ndx, "target index:", tag_ndx)
+
+# --- Load reference structure ---
+ref_file = f'./reference/dna_0_{ref_ndx}.gro'
+ref = mda.Universe(ref_file)
+ref_P = ref.select_atoms("name P and resname D*")
+
+# --- Load trajectory ---
+tag = mda.Universe(input_gro, input_trr)
+
+# Optional: sanity check for frame index
+n_frames = len(tag.trajectory)
+if tag_ndx < 0 or tag_ndx >= n_frames:
+    raise ValueError(f"Requested frame {tag_ndx} out of range [0, {n_frames-1}]")
+
+ts_ndx = 0
+
+# --- Loop over ALL frames (including last) ---
+for ts in tag.trajectory:
+    if ts.frame != tag_ndx:
+        continue
+
+    print("Using frame:", ts.frame)
+    tag_P = tag.select_atoms("name P and resname D*")
+
+    # RMSD and alignment
+    P_rmsd = rms.rmsd(ref_P.positions, tag_P.positions, superposition=True)
+    tag_P0 = tag_P.positions - tag_P.center_of_mass()
+    ref_P0 = ref_P.positions - ref_P.center_of_mass()
+    R, rmsd = align.rotation_matrix(tag_P0, ref_P0)
+    
+    tag.atoms.translate(-tag_P.center_of_mass())
+    tag.atoms.rotate(R)
+    tag.atoms.translate(ref_P.center_of_mass())
+
+    # Write the aligned frame
+    tag.atoms.write("dna_md_selected.gro")
+
+    # Compute per-phosphate distances and GDT
+    tag_P1 = tag.select_atoms("name P and resname D*")
+    
+    dist_list = []
+    for ref_atom, tag_atom in zip(ref_P, tag_P1):
+        atom_p1 = ref_atom.position
+        atom_p2 = tag_atom.position
+        dist = ((atom_p1[0]-atom_p2[0])**2 +
+                (atom_p1[1]-atom_p2[1])**2 +
+                (atom_p1[2]-atom_p2[2])**2) ** 0.5
+        dist_list.append(dist)
+
+    dist_array = np.asarray(dist_list)
+    gdts = []
+    for cutoff in cutoffs:
+        gdts.append((dist_array <= cutoff).sum() / float(len(dist_array)))
+    GDT = np.mean(gdts)
+    print(ts_ndx, gdts, ' mean score: %.4f' % GDT)
+
+    # If you only want this one frame, you can break here
+    break
+
+
+    
+
+
+
+
+
+
