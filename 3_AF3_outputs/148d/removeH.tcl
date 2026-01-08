acdeadf5fecb0a00fa3272c1ffa10b7db30fe456
s2315288 <148905237+s2315288@users.noreply.github.com>
2026-01-08 12:35:52 -0500

    Add files via upload

---
+++ 3_AF3_outputs/148d/removeH.tcl
@@ -0,0 +1,25 @@
+# strip_H.tcl
+# Usage:
+#   vmd -dispdev text -eofexit -e strip_H.tcl -args input.pdb output.pdb
+
+# Get arguments
+set in_file  [lindex $argv 0]
+set out_file [lindex $argv 1]
+
+# Load structure
+mol new $in_file waitfor all
+
+# Select all non-hydrogen atoms
+set sel [atomselect top "all and noh"]
+
+# Write out H-stripped PDB
+$sel writepdb $out_file
+
+# Clean up
+$sel delete
+mol delete top
+
+# Exit VMD
+exit
+
+
